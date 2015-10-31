aa1e90bccd3274bcedd9e71cba533bd731bb3dfa
Bastiaan Reijm <bastiaan.reijm@milvum.com>
2015-10-31 16:41:33 +0100

    Pie Chart List

--- red-analyzer/ChartsTableViewController.swift
+++ red-analyzer/ChartsTableViewController.swift
@@ -42,7 +42,7 @@ class ChartsTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifierLabel", forIndexPath: indexPath)
 
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        // cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
 
         return cell
     }
