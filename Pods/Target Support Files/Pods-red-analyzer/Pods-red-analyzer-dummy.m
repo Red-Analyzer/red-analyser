22e1214406bab374db0946d2306d984e22d1bbc2
Arvind Jagesser <arvind@milvum.com>
2015-10-31 12:01:44 +0100

    added charts pod

---
+++ Pods/Target Support Files/Pods-red-analyzer/Pods-red-analyzer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_red_analyzer : NSObject
+@end
+@implementation PodsDummy_Pods_red_analyzer
+@end
---
+++ Pods/Target Support Files/Pods-red-analyzer/Pods-red-analyzer-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_red_analyzerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_red_analyzerVersionString[];
+
